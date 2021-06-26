e70924c8dac345af45c3707d47cfc7e38e624ba7
Carlos_Lasprilla <carlos.380172791@ucaldas.edu.co>
2021-06-25 23:44:41 -0500

    Se crearon los cinco métodos de la api de mechanic y servicelist

--- MechanicsLaravel/app/Http/Controllers/MechanicApiController.php
+++ MechanicsLaravel/app/Http/Controllers/MechanicApiController.php
@@ -55,9 +55,10 @@ class MechanicApiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Mechanic $mechanic)
     {
-        //
+        //$mechanic = Mechanic::findOrFail($id);
+        return $mechanic;
     }
 
     /**
@@ -67,9 +68,14 @@ class MechanicApiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Mechanic $mechanic)
     {
-        //
+        $mechanic->update($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully update resource',
+        ]);
     }
 
     /**
@@ -78,8 +84,10 @@ class MechanicApiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Mechanic $mechanic)
     {
-        //
+        $mechanic->delete();
+
+        return response()->json(['status' => 'Delete Resource']);
     }
 }
